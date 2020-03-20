3fc3ab3c16c490f71bde88d9a1ef39a0b16bf9ff
ajinkya prabhu <ajinkyamprabhu1997@yahoo.com>
2020-03-20 12:20:06 +0530

    returns response now

--- google-service/src/main/java/controller/HttpRestController.java
+++ google-service/src/main/java/controller/HttpRestController.java
@@ -20,19 +20,21 @@ public class HttpRestController {
 	 */
     
     @GetMapping("getStocks")
-    public void getJSON() throws IOException {
+    public String getJSON() throws IOException {
 		final String SYM ="MSFT";
 		URL url = new URL("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=011MUAEI4T1XCEGL");
 		URLConnection connection = url.openConnection();
 		
 		InputStreamReader inputStream = new InputStreamReader(connection.getInputStream());
 		BufferedReader bufferedReader = new BufferedReader(inputStream);
-		
+		String response = "";
 		String line = bufferedReader.readLine();
 		while(line!=null) {
 			System.out.println(line);
 			line = bufferedReader.readLine();
+			response += line;
 		}
+		return response;
 	}
 
 
