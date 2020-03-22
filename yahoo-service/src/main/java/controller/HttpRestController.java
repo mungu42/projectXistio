d63f70eb3fc744bdfb9e08132a08e090c844ac88
johnty05 <johnty05>
2020-03-22 16:39:24 +0530

    Added yahoo service

--- yahoo-service/src/main/java/controller/HttpRestController.java
+++ yahoo-service/src/main/java/controller/HttpRestController.java
@@ -4,12 +4,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pojo.YahooDataPoint;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
 @RestController("/")
 public class HttpRestController {
 
     @GetMapping("getStocks")
-    public YahooDataPoint getStocks(){
-        return new YahooDataPoint("Field1",00.00,00.00);
-    }
+    public String getJSON() throws IOException {
+        final String SYM ="MSFT";
+        URL url = new URL("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=011MUAEI4T1XCEGL");
+        URLConnection connection = url.openConnection();
 
+        InputStreamReader inputStream = new InputStreamReader(connection.getInputStream());
+        BufferedReader bufferedReader = new BufferedReader(inputStream);
+        String response = "";
+        String line = bufferedReader.readLine();
+        while(line!=null) {
+            System.out.println(line);
+            line = bufferedReader.readLine();
+            response += line;
+        }
+        return response;
+    }
 }
