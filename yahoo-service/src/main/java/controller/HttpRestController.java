6a802884503504b10aea560d695361bfbeb89a12
ajinkya prabhu <ajinkyamprabhu1997@yahoo.com>
2020-03-24 13:41:28 +0530

    added yahoo and google response

--- yahoo-service/src/main/java/controller/HttpRestController.java
+++ yahoo-service/src/main/java/controller/HttpRestController.java
@@ -1,21 +1,14 @@
 package controller;
 
-import java.io.BufferedReader;
+import org.apache.http.HttpEntity;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.HashMap;
-import java.util.Map;
-
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import pojo.GoogleDataPoint;
 
 
 @RestController("/")
@@ -26,42 +19,17 @@ public class HttpRestController {
      * GoogleDataPoint("Field1",00.00,00.00); }
      */
 
-    private static Logger LOG = LoggerFactory.getLogger(HttpRestController.class);
-    private Map<String, Object> cache = new HashMap<String, Object>();
+    private AlphaVantageApi alphaVantageApi =  new AlphaVantageApi();
+    @GetMapping("getStocks")
+    public JSONObject getJSON() throws IOException, ParseException {
 
-    private final String LINK = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=%s&apikey=AZ35ESNS50ESUG75";
+        HttpEntity entity =  alphaVantageApi.getSymListData("MSFT");
 
-    @GetMapping("getStocks/{symbol}")
-    public Map getJSON(@PathVariable String symbol) throws IOException {
-        ObjectMapper mapperObj = new ObjectMapper();
-        String response = null;
-        HashMap<String, Object> resultMap = new HashMap<String, Object>();
-        if (!cache.containsKey(symbol)) {
-            LOG.info("link =>" + String.format(LINK, symbol));
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject = (JSONObject)jsonParser.parse(
+                new InputStreamReader(entity.getContent(), "UTF-8"));
 
-            URL url = new URL(String.format(LINK, symbol));
-
-
-            URLConnection connection = url.openConnection();
-
-
-
-            InputStreamReader inputStream = new InputStreamReader(connection.getInputStream());
-            BufferedReader bufferedReader = new BufferedReader(inputStream);
-            response = "";
-            String line = bufferedReader.readLine();
-            while (line != null) {
-                line = bufferedReader.readLine();
-                response += line;
-            }
-
-
-            cache.put(symbol,response);
-        }
-
-
-
-        return cache;
+        return jsonObject;
     }
 
 
