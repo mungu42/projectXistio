dcf2348664f294357d6f4a8899dce48693edda81
ajinkya prabhu <ajinkyamprabhu1997@yahoo.com>
2020-04-01 19:47:30 +0530

    fix google service

--- google-service/src/main/java/controller/HttpRestController.java
+++ google-service/src/main/java/controller/HttpRestController.java
@@ -33,8 +33,8 @@ public class HttpRestController {
         return jsonObject;
     }
 
-    @GetMapping("getAllStocks/{api_key}")
-    public List getAllData(@PathVariable("apiKey") String apiKey) throws IOException, ParseException {
+    @GetMapping("getAllStocks/{apiKey}")
+    public List getAllData(@PathVariable String apiKey) throws IOException, ParseException {
         return alphaVantageApi.getStoredSymbolResults(apiKey);
     }
 
